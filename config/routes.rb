ad222bf42525502101a41034250d203a51b99e58
Matthew_Rollo <rollomatthew@yahoo.com>
2013-07-17 13:28:07 -0500

    Work on routes3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 BMIExo::Application.routes.draw do
   
-  match '/home', to: 'welcome#index', via: 'get'
+  root 'welcome#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
