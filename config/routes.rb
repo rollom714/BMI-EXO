cd7783e015aa4c2c40494f1506813ac4593b4a94
Matthew_Rollo <rollomatthew@yahoo.com>
2013-07-19 10:22:52 -0500

    Adding contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BMIExo::Application.routes.draw do
   
   root 'welcome#index'
+  match '/contact', to: 'welcome#contact', via: 'get'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
