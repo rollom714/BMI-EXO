48f18ed2500e28e451451ece8e4edf1af4711af1
Matthew_Rollo <rollomatthew@yahoo.com>
2013-07-25 10:32:56 -0500

    Adding users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 BMIExo::Application.routes.draw do
   
+  get "users/new"
   resources :posts
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root 'welcome#index'
-  match '/contact', to: 'welcome#contact', via: 'get'
-  match '/news', to: 'post#index', via: 'get'
+  match '/contact', to: 'welcome#contact',  via: 'get'
+  match '/news',    to: 'post#index',       via: 'get'
+  match '/signup',  to: 'users#new',        via: 'get'
+  match '/signin',  to: 'sessions#new',     via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
