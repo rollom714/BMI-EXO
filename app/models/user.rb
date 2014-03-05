043f8c414b1255f19869a7a1a7f44ce8983e7db1
Matthew_Rollo <rollomatthew@yahoo.com>
2014-03-04 18:01:26 -0600

    Fixing research page and sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
 	before_save {self.email = email.downcase}
 	before_create :create_remember_token
 	
-	VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-	validates :email, presence:true, format: {with:VALID_EMAIL_REGEX}, uniqueness:{case_sensitive:false}
+	#VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i  Formats it so email is required
+	validates :email, presence:true, uniqueness:{case_sensitive:false} #,format: {with:VALID_EMAIL_REGEX}
 	has_secure_password
 	validates :password, length: { minimum: 6}
 	
